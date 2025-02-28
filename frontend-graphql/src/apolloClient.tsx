b1ce137bb564e9f7e1b440caf302b0af0be60ef1
Mathias002 <moussetmathias@gmail.com>
2025-02-28 16:20:38 +0100

    add set token = authorization

--- frontend-graphql/src/apolloClient.tsx
+++ frontend-graphql/src/apolloClient.tsx
@@ -1,6 +1,28 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-export const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
-  cache: new InMemoryCache()
+// Création du lien HTTP vers le serveur GraphQL
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000/graphql",
 });
+
+// Middleware pour ajouter le token dans l'Authorization
+export const authLink = setContext((_, { headers }) => {
+  // Récupère le token depuis le sessionStorage
+  const token = sessionStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Création du client Apollo avec les liens configurés
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default client;
